e1c8462a83ddd46c418aab6f56da83da6f5d7e7e
funmise <42499166+funmise@users.noreply.github.com>
2024-02-04 14:09:54 -0600

    adding base project structure

---
+++ backend/routes/index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+// new route for /hello
+router.get('/helloworld', function(req, res, next) {
+  res.send('Hello from Backend');
+});
+
+
+module.exports = router;
