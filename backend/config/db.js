3560b7fc2acf72a78507d17ff7e3b3b126c72493
Danielle Powell <136560369+DanielleDPowell@users.noreply.github.com>
2024-02-05 15:45:37 -0600

    Changed database engine dialect to absolute value
    Absolute Value = "postgres" (will not change in project)

--- backend/config/db.js
+++ backend/config/db.js
@@ -7,7 +7,7 @@ const sequelize = new Sequelize(
   process.env.DB_PASSWORD, 
   {
       host: process.env.DB_HOST,
-      dialect: process.env.DB_DIALECT,
+      dialect: "postgres",
       logging: false, // or console.log to see it all, probably have something to handle this
   }
 );
