5c139497726c658145a5765de78e89f2b7e68f15
lecegues <johnlecegues@gmail.com>
2024-02-05 19:24:50 -0600

    Removed postgres

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,14 +2,34 @@ require("dotenv").config();
 const { Sequelize } = require("sequelize");
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME, 
-  process.env.DB_USER, 
-  process.env.DB_PASSWORD, 
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
   {
-      host: process.env.DB_HOST,
-      dialect: process.env.DB_DIALECT,
-      logging: false, // or console.log to see it all, probably have something to handle this
+    host: process.env.DB_HOST,
+    dialect: "postgres",
+    logging: false, // or console.log to see it all, probably have something to handle this
+    retry: {
+      max: 10, // Maximum number of tries
+      backoffBase: 3000,
+      backoffExponent: 1.5,
+      match: [
+        /the database system is starting up/,
+        Sequelize.ConnectionError,
+        Sequelize.ConnectionRefusedError,
+        Sequelize.ConnectionTimedOutError,
+      ],
+    },
   }
 );
 
-  module.exports = { sequelize};
+// Check connection between the database and server
+sequelize
+  .authenticate()
+  .then(() => console.log("Connection has been established successfully."))
+  .catch((error) => {
+    console.error("Unable to connect to the database:", error);
+    process.exit(1); // Exit with a failure code
+  });
+
+module.exports = { sequelize };
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,19 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   test: {
     globals: true,
-    environment: 'jsdom',
-    setupFiles: './tests/setup.js',
+    environment: "jsdom",
+    setupFiles: "./tests/setup.js",
   },
   server: {
+    host: "0.0.0.0",
     port: 8080,
     proxy: {
       // Proxy requests matching /hello to the backend
-      '/hello': 'http://localhost:3000',
+      "/hello": "http://localhost:3000",
     },
   },
-})
+});
