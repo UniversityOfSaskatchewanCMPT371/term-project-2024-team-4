8549ee4ec077ab7b26d37b69c3c44927084c57a3
funmise <42499166+funmise@users.noreply.github.com>
2024-02-07 16:52:19 -0600

    finalized DB

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,35 +1,22 @@
 require("dotenv").config();
-const { Sequelize } = require("sequelize");
+const { DataSource } = require('typeorm');
+const { User } = require('../dist/user.entity');
+const {Catalogue, Site, Region, Period, Culture,BladeShape, BaseShape, HaftingShape,
+  CrossSection, Artifact, Material, ArtifactType, ProjectilePoint} = require('../dist/entity');
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASS,
-  {
-    host: process.env.DB_HOST,
-    dialect: "postgres",
-    logging: false, // or console.log to see it all, probably have something to handle this
-    retry: {
-      max: 10, // Maximum number of tries
-      backoffBase: 3000,
-      backoffExponent: 1.5,
-      match: [
-        /the database system is starting up/,
-        Sequelize.ConnectionError,
-        Sequelize.ConnectionRefusedError,
-        Sequelize.ConnectionTimedOutError,
-      ],
-    },
-  }
-);
 
-// Check connection between the database and server
-sequelize
-  .authenticate()
-  .then(() => console.log("Connection has been established successfully."))
-  .catch((error) => {
-    console.error("Unable to connect to the database:", error);
-    process.exit(1); // Exit with a failure code
-  });
+const dataSource = new DataSource({
+  type: process.env.DB_DIALECT,
+  host: process.env.DB_HOST,
+  port: 5432,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS, 
+  database: process.env.DB_NAME,
+  synchronize: true,
+  entities: [ User, Catalogue, Site, Region, Period, Culture,BladeShape, BaseShape, HaftingShape,
+  CrossSection, Artifact, Material, ArtifactType, ProjectilePoint ],
+});
 
-module.exports = { sequelize };
+dataSource.initialize().then(() => console.log('connected to DB succesfully!'));
+
+module.exports = dataSource;
