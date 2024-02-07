3b73d38566de11df4151de409c1d03a2dfbe3cf7
funmise <42499166+funmise@users.noreply.github.com>
2024-02-07 15:28:01 -0600

    modified user model and updated npm start script

--- backend/src/user.entity.ts
+++ backend/src/user.entity.ts
@@ -1,13 +1,27 @@
-import { Entity, Column, PrimaryGeneratedColumn } from "typeorm"
+import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
 
+// Define the RoleType enum
+enum RoleType {
+    BASIC = "basic",
+    ADMIN = "admin"
+}
+
+// User entity definition
 @Entity()
 export class User {
     @PrimaryGeneratedColumn()
-    id: number
+    id: number;
 
     @Column()
-    firstName: string
+    userName: string;
 
-    @Column()
-    lastName: string
+    @Column("text")
+    password: string;
+
+    @Column({
+        type: "enum",
+        enum: RoleType,
+        default: RoleType.BASIC
+    })
+    role: RoleType;
 }
