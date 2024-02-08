974fbda3ff84a2e111bd78b20581fe43620dedb6
Kelly Neuda <kvn904@mail.usask.ca>
2024-02-07 19:51:35 -0600

    Updated login modal labels, placeholder texts, and text field types

--- frontend/src/components/LoginModal.jsx
+++ frontend/src/components/LoginModal.jsx
@@ -9,33 +9,39 @@ function LoginModal(props) {
       size="md"
       aria-labelledby="contained-modal-title-vcenter"
       centered
+      animation={false}
     >
       <Modal.Header closeButton>
         <Modal.Title id="contained-modal-title-vcenter">
-          Modal heading
+          Login
         </Modal.Title>
       </Modal.Header>
       <Modal.Body>
       <Form>
-        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-            <Form.Label>Email address</Form.Label>
+        <Form.Group className="mb-3" controlId="loginFormUsername">
+            <Form.Label>Username</Form.Label>
             <Form.Control
             type="email"
-            placeholder="name@example.com"
+            placeholder="Username"
             autoFocus
             />
         </Form.Group>
-        <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-            <Form.Label>Example textarea</Form.Label>
-            <Form.Control as="textarea" rows={3} />
+        <Form.Group className="mb-3" controlId="loginFormPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
         </Form.Group>
         </Form>
       </Modal.Body>
       <Modal.Footer>
-        <Button onClick={props.onHide}>Close</Button>
+        <Button variant="secondary" onClick={props.onHide}>
+          Close
+        </Button>
+        <Button variant="primary" onClick={props.onHide}>
+          Login
+        </Button>
       </Modal.Footer>
     </Modal>
   );
 }
 
-export default LoginModal;
+export default LoginModal
