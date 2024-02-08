1eca35b26fa7b06de39485f0ae8524b48750a144
m-jewel <wzt206@usask.ca>
2024-02-07 22:49:28 -0600

    Get specific catalogue and Update specific catalogue was implemented. Tested using postman.

--- backend/routes/catalogues.js
+++ backend/routes/catalogues.js
@@ -26,17 +26,58 @@ router.get("/", async (req, res) => {
 
 // POST: Create a new catalogue
 router.post("/", async (req, res) => {
-    const { name, description } = req.body;
-    try {
-      const catalogueRepository = await myDatabase.getRepository(Catalogue);
-      const newCatalogue = catalogueRepository.create({ name, description });
-      await catalogueRepository.save(newCatalogue);
-      res.status(201).json(newCatalogue);
-    } catch (error) {
-      console.error("Error creating new catalogue:", error);
-      res.status(400).json({ error: error.message });
+  const { name, description } = req.body;
+  try {
+    const catalogueRepository = await myDatabase.getRepository(Catalogue);
+    const newCatalogue = catalogueRepository.create({ name, description });
+    await catalogueRepository.save(newCatalogue);
+    res.status(201).json(newCatalogue);
+  } catch (error) {
+    console.error("Error creating new catalogue:", error);
+    res.status(400).json({ error: error.message });
+  }
+});
+
+// GET: Fetch a catalogue by ID
+router.get("/:id", async (req, res) => {
+  try {
+    const catalogueRepository = await myDatabase.getRepository(Catalogue);
+    const catalogue = await catalogueRepository.findOneBy({
+      id: parseInt(req.params.id),
+    });
+
+    if (catalogue) {
+      res.json(catalogue);
+    } else {
+      res.status(404).send("Catalogue not found");
     }
-  });
-  
+  } catch (error) {
+    console.error("Error fetching catalogue:", error);
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// PUT: Update an existing catalogue
+router.put("/:id", async (req, res) => {
+  const { id } = req.params;
+  const { name, description } = req.body;
+  try {
+    const catalogueRepository = await myDatabase.getRepository(Catalogue);
+    let catalogueToUpdate = await catalogueRepository.findOneBy({
+      id: parseInt(id),
+    });
+    if (catalogueToUpdate) {
+      catalogueToUpdate.name = name;
+      catalogueToUpdate.description = description;
+      await catalogueRepository.save(catalogueToUpdate);
+      res.json(catalogueToUpdate);
+    } else {
+      res.status(404).json({ message: "Catalogue not found" });
+    }
+  } catch (error) {
+    console.error("Error updating catalogue:", error);
+    res.status(500).json({ error: error.message });
+  }
+});
 
 module.exports = router;
