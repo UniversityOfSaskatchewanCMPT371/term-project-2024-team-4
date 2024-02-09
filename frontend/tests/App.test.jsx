71dc50d6783deb375e3cf8752000d8bdf1603141
Kelly Neuda <kvn904@mail.usask.ca>
2024-02-08 23:11:16 -0600

    Added frontend unit test cases in App.test.jsx

--- frontend/tests/App.test.jsx
+++ frontend/tests/App.test.jsx
@@ -1,12 +1,51 @@
-import { render, screen } from '@testing-library/react';
-import App from '../src/App';
+import { screen, render, cleanup, fireEvent } from '@testing-library/react'
+import Sidebar from '../src/components/Sidebar.jsx';
 
-describe('App', () => {
-	it('renders headline', () => {
-		render(<App title="React" />);
+/* RUN 'npx vitest' in terminal to run unit tests */
 
-		screen.debug();
+test('Render Sidebar component correctly', () => {
+	render(<Sidebar />);
 
-		// check if App components renders headline
-	});
+	// Test if title is present on Sidebar
+	const titleElement = screen.getByText('Projectile');
+  	expect(titleElement).toBeInTheDocument();
+
+	// Test if Home nav link is present on Sidebar
+	const homeNav = screen.getByText('Home');
+	expect(homeNav).toBeInTheDocument();
+
+	// Test if Connect nav link is present on Sidebar
+	const connectNav = screen.getByText('Connect');
+	expect(connectNav).toBeInTheDocument();
+
+	// Test if Statistics nav link is present on Sidebar
+	const statisticsNav = screen.getByText('Statistics');
+	expect(statisticsNav).toBeInTheDocument();
+
+	// Test if Data Management nav link is present on Sidebar
+	const dataManagementNav = screen.getByText('Data Management');
+	expect(dataManagementNav).toBeInTheDocument();
+
+	// Test if Settings nav link is present on Sidebar
+	const settingsNav = screen.getByText('Settings');
+	expect(settingsNav).toBeInTheDocument();
+
+	// Test if Login nav link is present on Sidebar
+	const loginNav = screen.getByText('Login');
+	expect(loginNav).toBeInTheDocument();
+});
+
+test('Render LoginModal component correctly', async () => {
+	render(<Sidebar />);
+
+	// Click on the Login Sidebar button to open the LoginModal
+	await fireEvent.click(screen.getByText('Login'));
+
+	// Check if username field is present in modal
+	const usernameField = await screen.getByPlaceholderText('Username');
+	expect(usernameField).toBeInTheDocument();
+
+	// Check if password field is present in modal
+	const passwordField = await screen.getByPlaceholderText('Password');
+	expect(passwordField).toBeInTheDocument();
 });
--- frontend/tests/setup.js
+++ frontend/tests/setup.js
@@ -1,6 +1,7 @@
 import { expect, afterEach } from 'vitest';
 import { cleanup } from '@testing-library/react';
 import * as matchers from "@testing-library/jest-dom/matchers";
+import '@testing-library/jest-dom';
 
 expect.extend(matchers);
 
