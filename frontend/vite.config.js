f9fff5385c34aa81a3386af830af545375b0e616
lecegues <johnlecegues@gmail.com>
2024-02-12 00:59:36 -0600

    Small changes to vite-config file in reference to pipeline failure

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,4 +1,4 @@
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
@@ -7,7 +7,7 @@ export default defineConfig({
   test: {
     globals: true,
     environment: "jsdom",
-    setupFiles: "./tests/setup.js",
+    setupFiles: ["/tests/setup.js"],
   },
   server: {
     watch: {
