1951d376af37e0c08da41cc069a12ce979db8aaf
lecegues <johnlecegues@gmail.com>
2024-02-16 13:40:40 -0600

    Ran frontend through ESLint and Prettier & fixed code style issues

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,5 +1,5 @@
-import { Component } from 'react';
-import Sidebar from './Sidebar';
+import { Component } from "react";
+import Sidebar from "./Sidebar";
 
 class App extends Component {
 	constructor(props) {
@@ -10,36 +10,26 @@ class App extends Component {
 		return (
 			<>
 				<div className="row">
-					<div className='col-3'>
+					<div className="col-3">
 						<Sidebar />
 					</div>
-					<div className='col'>
-						<div className='container'>
+					<div className="col">
+						<div className="container">
 							<h2>&lt; Catalogue &lt;Museum of Anthropology&gt; / X Farm</h2>
 							<div className="row">
-								<div className="col">
-									Row 2 Col 1
-								</div>
-								<div className="col">
-									Row 2 Col 2
-								</div>
+								<div className="col">Row 2 Col 1</div>
+								<div className="col">Row 2 Col 2</div>
 							</div>
 							<div className="row">
-								<div className="col">
-									Row 3 Col 1
-								</div>
-								<div className="col">
-									Row 3 Col 2
-								</div>
-								<div className="col">
-									Row 3 Col 3
-								</div>
+								<div className="col">Row 3 Col 1</div>
+								<div className="col">Row 3 Col 2</div>
+								<div className="col">Row 3 Col 3</div>
 							</div>
 						</div>
 					</div>
 				</div>
 			</>
-		)
+		);
 	}
 }
 
