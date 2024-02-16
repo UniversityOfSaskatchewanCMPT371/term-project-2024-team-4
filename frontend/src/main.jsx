9ab74abbdf0969be330aee01c46d963c0379c48e
Kelly Neuda <kvn904@mail.usask.ca>
2024-02-16 00:00:23 -0600

    Removed import bootstrap.min.css in main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./components/App";
 import './index.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
