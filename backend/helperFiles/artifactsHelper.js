85d16e7a9fe554975172f8c11b4384dd29726625
brj496 <brj496@usask.ca>
2024-02-27 20:10:38 -0600

    made changes to theses routes, and create helper functions so that the database calls may be made internally.

---
+++ backend/helperFiles/artifactsHelper.js
@@ -0,0 +1,197 @@
+const { Artifact, Site, ArtifactType } = require("../dist/entity");
+const myDatabase = require("../config/db");
+
+/**
+ * Creates a new artifact in the database
+ * @param {*} req - the request
+ */
+async function newArtifact(req) {
+	// Extract the information from the request body
+	const {
+		name,
+		location,
+		description,
+		dimensions,
+		photo,
+		siteId,
+		artifactTypeId,
+		subtype,
+	} = req.body;
+	try {
+		// Use database connection to get the necessary repositories
+		const artifactRepository = await myDatabase.getRepository(Artifact);
+		const site = await myDatabase.getRepository(Site).findOneBy({ id: siteId });
+		const artifactType = await myDatabase
+			.getRepository(ArtifactType)
+			.findOneBy({ id: artifactTypeId });
+		// If the site or artifactType is not found, return a 404
+		if (!site || !artifactType) {
+			return "Site or ArtifactType not found";
+			//return res.json({ message: "Site or ArtifactType not found" });
+		}
+
+		// If the site and artifactType exist, creates a new Artifact entity with the given information
+		const artifact = artifactRepository.create({
+			name,
+			location,
+			description,
+			dimensions,
+			photo,
+			site,
+			artifactType,
+			subtype,
+		});
+		// Save the new Artifact entity to the database
+		await artifactRepository.save(artifact);
+		return artifact;
+		//res.json(artifact);
+	} catch (error) {
+		console.error("Error creating Artifact:", error);
+		return error;
+		//res.json({ error: error.message });
+	}
+}
+
+/**
+ * Gets all artifacts in the database.
+ */
+async function getAllArtifacts() {
+	try {
+		const artifactRepository = await myDatabase.getRepository(Artifact);
+		const artifacts = await artifactRepository.find({
+			relations: ["site", "artifactType"],
+		});
+		if (Artifact) {
+			return artifacts;
+			//res.json(artifacts);
+		} else {
+			return "Artifacts not found";
+		}
+	} catch (error) {
+		console.error("Error fetching Artifacts:", error);
+		return error;
+		//res.json({ error: error.message });
+	}
+}
+
+/**
+ * get a single artifact that matches the given id
+ * @param {*} req - the id requested
+ */
+async function getArtifactFromId(req) {
+	const { id } = req.params;
+	try {
+		const artifact = await myDatabase.getRepository(Artifact).findOne({
+			where: { id },
+			relations: ["site", "artifactType"],
+		});
+		if (!artifact) {
+			return "Artifact not found";
+			//res.json({ message: "Artifact not found" });
+		} else {
+			return artifact;
+			//res.json(artifact);
+		}
+	} catch (error) {
+		console.error("Error fetching Artifacts:", error);
+		return error;
+		//res.json({ error: error.message });
+	}
+}
+
+/**
+ * update a given artifact with new given information
+ * @param {*} req - the request
+ */
+async function updateArtifact(req) {
+	const { id } = req.params;
+	const {
+		name,
+		location,
+		description,
+		dimensions,
+		photo,
+		siteId,
+		artifactTypeId,
+		subtype,
+	} = req.body;
+	try {
+		const artifactRepository = await myDatabase.getRepository(Artifact);
+		let artifact = await artifactRepository.findOneBy({ id });
+
+		if (!artifact) {
+			return "Artifact not found";
+			//return res.json({ message: "Artifact not found" });
+		}
+
+		if (siteId) {
+			const site = await myDatabase
+				.getRepository(Site)
+				.findOneBy({ id: siteId });
+			if (!site) {
+				return "Site not found";
+				//return res.json({ message: "Site not found" });
+			}
+			artifact.site = site;
+		}
+
+		if (artifactTypeId) {
+			const artifactType = await myDatabase
+				.getRepository(ArtifactType)
+				.findOneBy({ id: artifactTypeId });
+			if (!artifactType) {
+				return "ArtifactType not found";
+				//return res.json({ message: "ArtifactType not found" });
+			}
+			artifact.artifactType = artifactType;
+		}
+
+		// Update fields
+		artifact.name = name;
+		artifact.location = location;
+		artifact.description = description;
+		artifact.dimensions = dimensions;
+		artifact.photo = photo;
+		artifact.subtype = subtype;
+
+		await artifactRepository.save(artifact);
+		return artifact;
+		//res.json(artifact);
+	} catch (error) {
+		console.error("Error updating Artifact:", error);
+		return error;
+		//res.json({ error: error.message });
+	}
+}
+
+/**
+ * deletes the artifact that matches the given id.
+ * @param {*} req - the request
+ */
+async function deleteArtifact(req) {
+	const { id } = req.params;
+	try {
+		const artifactRepository = await myDatabase.getRepository(Artifact);
+		const deleteResult = await artifactRepository.delete(id);
+
+		if (deleteResult.affected > 0) {
+			return;
+			//res.send();
+		} else {
+			return "Artifact not found";
+			//res.json({ message: "Artifact not found" });
+		}
+	} catch (error) {
+		console.error("Error deleting Artifact:", error);
+		return error;
+		//res.json({ error: error.message });
+	}
+}
+
+module.exports = {
+	newArtifact,
+	getAllArtifacts,
+	getArtifactFromId,
+	updateArtifact,
+	deleteArtifact,
+};
---
+++ backend/helperFiles/projectilePointsHelper.js
@@ -0,0 +1,230 @@
+const {
+	ProjectilePoint,
+	Culture,
+	BladeShape,
+	BaseShape,
+	HaftingShape,
+	CrossSection,
+} = require("../dist/entity");
+const myDatabase = require("../config/db");
+
+/**
+ *
+ * @param {*} req
+ */
+async function newProjectilePoint(req) {
+	const {
+		name,
+		location,
+		description,
+		dimensions,
+		photo,
+		siteId,
+		artifactTypeId,
+		cultureId,
+		bladeShapeId,
+		baseShapeId,
+		haftingShapeId,
+		crossSectionId,
+	} = req.body;
+
+	try {
+		// Fetch related entities by their IDs
+		const culture = await myDatabase
+			.getRepository(Culture)
+			.findOneBy({ id: cultureId });
+		const bladeShape = await myDatabase
+			.getRepository(BladeShape)
+			.findOneBy({ id: bladeShapeId });
+		const baseShape = await myDatabase
+			.getRepository(BaseShape)
+			.findOneBy({ id: baseShapeId });
+		const haftingShape = await myDatabase
+			.getRepository(HaftingShape)
+			.findOneBy({ id: haftingShapeId });
+		const crossSection = await myDatabase
+			.getRepository(CrossSection)
+			.findOneBy({ id: crossSectionId });
+
+		// Then create a new instance of ProjectilePoint with the provided and fetched data
+		const projectilePoint = new ProjectilePoint();
+		projectilePoint.name = name;
+		projectilePoint.location = location;
+		projectilePoint.description = description;
+		projectilePoint.dimensions = dimensions;
+		projectilePoint.photo = photo;
+		projectilePoint.site = { id: siteId };
+		projectilePoint.artifactType = { id: artifactTypeId };
+		projectilePoint.culture = culture;
+		projectilePoint.bladeShape = bladeShape;
+		projectilePoint.baseShape = baseShape;
+		projectilePoint.haftingShape = haftingShape;
+		projectilePoint.crossSection = crossSection;
+
+		// Set properties directly and through fetched entities
+		// Save the new instance to the database
+		await myDatabase.getRepository(ProjectilePoint).save(projectilePoint);
+		return projectilePoint;
+		//res.status(201).json(projectilePoint);
+	} catch (error) {
+		console.error("Error creating ProjectilePoint:", error);
+		return error;
+		//res.status(500).json({ error: error.message });
+	}
+}
+
+/**
+ *
+ */
+async function getAllProjectilePoints() {
+	try {
+		const projectilePoints = await myDatabase
+			.getRepository(ProjectilePoint)
+			.find({
+				relations: [
+					"culture",
+					"bladeShape",
+					"baseShape",
+					"haftingShape",
+					"crossSection",
+				],
+			});
+		return projectilePoints;
+		//res.json(projectilePoints);
+	} catch (error) {
+		console.error("Error fetching ProjectilePoints:", error);
+		return error;
+		//res.json({ error: error.message });
+	}
+}
+
+/**
+ *
+ * @param {*} req
+ */
+async function getProjectilePointFromId(req) {
+	const { id } = req.params;
+	try {
+		const projectilePoint = await myDatabase
+			.getRepository(ProjectilePoint)
+			.findOne({
+				where: { id },
+				relations: [
+					"culture",
+					"bladeShape",
+					"baseShape",
+					"haftingShape",
+					"crossSection",
+				],
+			});
+		if (!projectilePoint) {
+			return "ProjectilePoint not found";
+			//return res.json({ message: "ProjectilePoint not found" });
+		}
+		return projectilePoint;
+		//res.json(projectilePoint);
+	} catch (error) {
+		console.error("Error fetching ProjectilePoint:", error);
+		return error;
+		//res.json({ error: error.message });
+	}
+}
+
+/**
+ *
+ * @param {*} req
+ */
+async function updateProjectilePoint(req) {
+	const { id } = req.params;
+	const {
+		name,
+		location,
+		description,
+		dimensions,
+		photo,
+		siteId,
+		artifactTypeId,
+		cultureId,
+		bladeShapeId,
+		baseShapeId,
+		haftingShapeId,
+		crossSectionId,
+	} = req.body;
+
+	try {
+		// Fetch the existing ProjectilePoint entity
+		let projectilePoint = await myDatabase
+			.getRepository(ProjectilePoint)
+			.findOneBy({ id });
+		if (!projectilePoint) {
+			return "ProjectilePoint not found";
+			//return res.json({ message: "ProjectilePoint not found" });
+		}
+
+		// Update the basic Artifact properties
+		projectilePoint.name = name;
+		projectilePoint.location = location;
+		projectilePoint.description = description;
+		projectilePoint.dimensions = dimensions;
+		projectilePoint.photo = photo;
+		projectilePoint.site = { id: siteId };
+		projectilePoint.artifactType = { id: artifactTypeId };
+
+		// Fetch and set the related entities
+		projectilePoint.culture = await myDatabase
+			.getRepository(Culture)
+			.findOneBy({ id: cultureId });
+		projectilePoint.bladeShape = await myDatabase
+			.getRepository(BladeShape)
+			.findOneBy({ id: bladeShapeId });
+		projectilePoint.baseShape = await myDatabase
+			.getRepository(BaseShape)
+			.findOneBy({ id: baseShapeId });
+		projectilePoint.haftingShape = await myDatabase
+			.getRepository(HaftingShape)
+			.findOneBy({ id: haftingShapeId });
+		projectilePoint.crossSection = await myDatabase
+			.getRepository(CrossSection)
+			.findOneBy({ id: crossSectionId });
+
+		// Save the updated entity
+		await myDatabase.getRepository(ProjectilePoint).save(projectilePoint);
+
+		return projectilePoint;
+		//res.json(projectilePoint);
+	} catch (error) {
+		console.error("Error updating ProjectilePoint:", error);
+		return error;
+		//res.json({ error: error.message });
+	}
+}
+
+/**
+ *
+ * @param {*} req
+ */
+async function deleteProjectilePoint(req) {
+	const { id } = req.params;
+	try {
+		const result = await myDatabase.getRepository(ProjectilePoint).delete(id);
+		if (result.affected > 0) {
+			return;
+			//res.send();
+		} else {
+			return "ProjectilePoint not found";
+			//res.json({ message: "ProjectilePoint not found" });
+		}
+	} catch (error) {
+		console.error("Error deleting ProjectilePoint:", error);
+		return error;
+		//res.json({ error: error.message });
+	}
+}
+
+module.exports = {
+	newProjectilePoint,
+	getAllProjectilePoints,
+	getProjectilePointFromId,
+	updateProjectilePoint,
+	deleteProjectilePoint,
+};
