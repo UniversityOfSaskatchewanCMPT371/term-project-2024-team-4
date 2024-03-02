ff5d2e34edc5d100431a41cef62c9d7baa5092d4
funmise <42499166+funmise@users.noreply.github.com>
2024-03-02 11:43:57 -0600

    fixed linting issues

--- backend/config/initialize.js
+++ backend/config/initialize.js
@@ -1,27 +1,30 @@
 const dataSource = require("./db");
-const { Catalogue } = require("../dist/entity")
-
+const { Catalogue } = require("../dist/entity");
 
 async function initializeDefaultCatalogue() {
 	try {
-	  
-	  const catalogueRepository = dataSource.getRepository(Catalogue);
-	  const defaultCatalogueExists = await catalogueRepository.findOneBy({ name: "Default Catalogue" });
-	  
-	  if (!defaultCatalogueExists) {
-		console.log("Creating default catalogue...");
-		const defaultCatalogue = catalogueRepository.create({
-		  name: "Default Catalogue",
-		  description: "This is the default catalogue."
+		const catalogueRepository = dataSource.getRepository(Catalogue);
+		const defaultCatalogueExists = await catalogueRepository.findOneBy({
+			name: "Default Catalogue",
 		});
-		await catalogueRepository.save(defaultCatalogue);
-		console.log("Default catalogue created.");
-	  } else {
-		console.log("Default catalogue already exists.");
-	  }
-	} catch (error) {
-	  console.error("Failed to connect to the database or create default catalogue:", error);
-	}
-  }
 
-  module.exports = initializeDefaultCatalogue;
+		if (!defaultCatalogueExists) {
+			console.log("Creating default catalogue...");
+			const defaultCatalogue = catalogueRepository.create({
+				name: "Default Catalogue",
+				description: "This is the default catalogue.",
+			});
+			await catalogueRepository.save(defaultCatalogue);
+			console.log("Default catalogue created.");
+		} else {
+			console.log("Default catalogue already exists.");
+		}
+	} catch (error) {
+		console.error(
+			"Failed to connect to the database or create default catalogue:",
+			error,
+		);
+	}
+}
+
+module.exports = initializeDefaultCatalogue;
