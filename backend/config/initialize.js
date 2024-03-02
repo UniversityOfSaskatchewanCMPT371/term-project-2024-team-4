f560e9ff7bd8d1c1a6cdc1dfb7f9de0d27314d2c
funmise <42499166+funmise@users.noreply.github.com>
2024-03-01 19:31:34 -0600

    moved init of db to www, and added default catalogue

---
+++ backend/config/initialize.js
@@ -0,0 +1,27 @@
+const dataSource = require("./db");
+const { Catalogue } = require("../dist/entity")
+
+
+async function initializeDefaultCatalogue() {
+	try {
+	  
+	  const catalogueRepository = dataSource.getRepository(Catalogue);
+	  const defaultCatalogueExists = await catalogueRepository.findOneBy({ name: "Default Catalogue" });
+	  
+	  if (!defaultCatalogueExists) {
+		console.log("Creating default catalogue...");
+		const defaultCatalogue = catalogueRepository.create({
+		  name: "Default Catalogue",
+		  description: "This is the default catalogue."
+		});
+		await catalogueRepository.save(defaultCatalogue);
+		console.log("Default catalogue created.");
+	  } else {
+		console.log("Default catalogue already exists.");
+	  }
+	} catch (error) {
+	  console.error("Failed to connect to the database or create default catalogue:", error);
+	}
+  }
+
+  module.exports = initializeDefaultCatalogue;
