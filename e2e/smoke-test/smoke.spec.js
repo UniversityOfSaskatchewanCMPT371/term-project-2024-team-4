e70fa38693780065545030ebf648789e3a4b872a
lecegues <johnlecegues@gmail.com>
2024-03-03 20:13:58 -0600

    Small mistake adding "\" instead of quotes in test case

--- e2e/smoke-test/smoke.spec.js
+++ e2e/smoke-test/smoke.spec.js
@@ -10,7 +10,7 @@ test('has title', async ({ page }) => {
     await page.goto('http://localhost:8080');
   
     // Expect a title "to contain" a substring.
-    await expect(page).toHaveTitle(/Vite + React/);
+    await expect(page).toHaveTitle("Vite + React");
   });
 
 /*
