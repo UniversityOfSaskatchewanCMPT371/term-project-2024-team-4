d447144eed7fd1fcda79228010b617ff0292ddd2
John Lecegues <68977770+lecegues@users.noreply.github.com>
2024-03-03 20:17:27 -0600

    Introduce Playwright for smoke tests in the CI Pipeline (#89)
    * Initialized playwright and created a new root folder for e2e tests
    
    * Added a step for playwright smoke tests
    
    * Added steps to run the frontend and backend services before playwright tests
    
    * Small addition to start frontend/backend in background during Playwright steps
    
    * Added a smoke-test file with planned smoke tests & an example test

---
+++ e2e/smoke-test/smoke.spec.js
@@ -0,0 +1,41 @@
+// @ts-check
+const { test, expect } = require('@playwright/test');
+/**
+ * This file will host our smoke tests to ensure that
+ * the main functionality of the system works.
+ * Main Functionality is based off our Features (Requirements Specs)
+ */
+
+test('has title', async ({ page }) => {
+    await page.goto('http://localhost:8080');
+  
+    // Expect a title "to contain" a substring.
+    await expect(page).toHaveTitle("Vite + React");
+  });
+
+/*
+Admin Login
+    - Invalid Credentials: cannot login
+        - Test admin-only endpoint before login
+    - Valid Credentials: can log in
+        - Test admin-only endpoint after login
+*/
+
+/*
+Catalog Management
+    - Can add new artifact
+    - Can modify the artifact
+    - Can delete the artifact
+*/
+
+/*
+Import/Export
+*/
+
+/*
+Data Aggregation
+*/
+
+/*
+Search/Filter
+*/
