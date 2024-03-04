751f9f91efce5b44f7e103b7427ca29d1317c7c2
lecegues <johnlecegues@gmail.com>
2024-03-03 22:53:04 -0600

    Fixed package-lock.json files

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -8,9 +8,6 @@ import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
-import Catalogue from "./components/Catalogue1";
-import AddProjectile from "./components/AddProjectile";
-import Site from "./components/Site";
 
 const theme = createTheme({
 	palette: {
