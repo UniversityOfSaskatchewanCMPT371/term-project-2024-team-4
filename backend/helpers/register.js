02cd0cbdb4ab9c7d6439d74a5ecc18dd80859e1c
Dhruv Chaudhari <qdf029@usask.ca>
2024-03-05 23:29:25 -0600

    added api docs

--- backend/helpers/register.js
+++ backend/helpers/register.js
@@ -3,6 +3,17 @@ const dataSource = require("../config/db");
 const bcrypt = require("bcrypt");
 const { logger } = require("../config/logger");
 
+/**
+ * registerUser
+ *
+ * Purpose: Register a new user with hardcoded username and password.
+ *
+ * Pre-conditions: None
+ *
+ * Post-conditions:
+ * - Registers a new user with the hardcoded username and password.
+ * - Returns void.
+ */
 const registerUser = async () => {
 	const Users = await dataSource.getRepository(User);
 	// hardcoded data
