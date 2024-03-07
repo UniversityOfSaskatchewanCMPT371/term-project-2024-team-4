5ab66870b5022f81abc807d77956f0954c68b549
DcGitLoom <155926622+DcGitLoom@users.noreply.github.com>
2024-03-06 19:47:17 -0600

    Feature/login backend (#65)
    * Unit tests Added
    
    * User Login with unit testing completed
    
    * new changes
    
    * helper file
    
    * add frontend for login changes
    
    * env file deleted
    
    * ci pipeline error solved
    
    * unit test error solved
    
    * Remove unnecessary package*.json files to address issue #67
    
    * ci pipeline test
    
    * ci pipeline fix test
    
    * prettier fix
    
    * small change to test pipeline
    
    * Update to build the tsc files before testing backend
    
    * Made user context fro admin credentials and some small bugs solved
    
    * Eslint and prettier fixed
    
    * prettier fix
    
    * deleted unwanted env file
    
    * Added feature-1 tests as playwright smoke tests
    
    * Changed `smoke-test` step name to better reflect what is done in the step
    
    * added api docs and user validations
    
    * added api docs
    
    * Remove frontend user validation not required for login
    
    * Updated login smoke-test to be more robust
    
    - tests HTTP response code rather than popup message
    
    ---------
    
    Co-authored-by: Dhruv Chaudhari <qdf029@usask.ca>
    Co-authored-by: lecegues <johnlecegues@gmail.com>
    Co-authored-by: John Lecegues <68977770+lecegues@users.noreply.github.com>

--- e2e/smoke-test/smoke.spec.js
+++ e2e/smoke-test/smoke.spec.js
@@ -1,17 +1,21 @@
 // @ts-check
-const { test, expect } = require('@playwright/test');
+const { test, expect} = require('@playwright/test');
 /**
  * This file will host our smoke tests to ensure that
  * the main functionality of the system works.
  * Main Functionality is based off our Features (Requirements Specs)
  */
 
+/*
+Test that you can succesfully go to page with proper title (e.g. page exists)
+*/
 test('has title', async ({ page }) => {
     await page.goto('http://localhost:8080');
   
-    // Expect a title "to contain" a substring.
+    // Expect the correct title
     await expect(page).toHaveTitle("Vite + React");
   });
+
 
 /*
 Admin Login
@@ -20,6 +24,67 @@ Admin Login
     - Valid Credentials: can log in
         - Test admin-only endpoint after login
 */
+test.describe("Login Smoke Tests", () => {
+
+    // test to determine proper response HTTP status codes upon login attempt
+    test('Login process with failure and success scenarios', async ({ page }) => {
+        // Navigate to login page
+        await page.goto("http://localhost:8080");
+
+        // Find and press Login Button
+        const loginButton = page.locator('text=Login');
+        await expect(loginButton).toBeVisible();
+        await loginButton.click();
+
+        // Define credentials for testing 
+        // TC01 to TC08
+        const credentials = [
+            { username: 'admin123#.;', password: "admin"},
+            { username: 'ADMIN', password: "admin"},
+            { username: 'admin', password: "admin123#.;"},
+            { username: 'admin', password: "ADMIN"},
+            { username: '', password: ""},
+            { username: 'admin', password: ""},
+            { username: '', password: "admin"},
+            { username: 'admin', password: "admin"},
+
+        ];
+
+        // Use credentials to attempt login
+        for (const credential of credentials) {
+
+            // Fill the inputs
+            await page.fill('input[name="username"]', credential.username);
+            await page.fill('input[name="password"]', credential.password);
+
+            await page.waitForLoadState('networkidle')
+
+            // Submit login 
+            var [response] = await Promise.all([
+                page.waitForResponse(resp => resp.url().includes('/users') && resp.request().method() == "POST"),
+                await page.locator("[type=submit]").click()
+            ]);
+            
+            // Expect the right response
+            try {
+                var responseStatus = response.status();
+
+                if (credential.username === 'admin' && credential.password === 'admin'){
+                    // expect succesful response
+                    await expect(responseStatus).toBe(200);
+                }
+                else{
+                    await expect([400,401]).toContain(responseStatus);
+                }
+            }
+            catch (error){
+                console.error("Error parsing response JSON", error);
+            }
+            
+            
+        }
+    })
+});
 
 /*
 Catalog Management
