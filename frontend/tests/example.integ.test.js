1420d8ca80b39eb2fcf968dbe6de3640b13ef84b
John Lecegues <68977770+lecegues@users.noreply.github.com>
2024-03-11 14:06:42 -0600

    Updated testing directories and file names (#93)
    * Changed testing directories and scripts for better management
    
    Scripts:
    - 'npm run unit-test': runs all unit tests
    - 'npm run integ-test': runs all integration tests
    
    File Changes:
    - all unit tests must end with ".unit.test"
    - all integration tests must end with ".integ.test"
    - e2e directory changed to /testing directory
        - holds all system-wide tests (e2e, system, load, performance)
    
    * Renamed existing tests to match last commit naming conventions
    
    * Changed workflow file to accomodate for the testing directory
    
    * Updated workflow to include unit/integration test checks
    
    * Added example integration tests to initially pass the pipeline tests

---
+++ frontend/tests/example.integ.test.js
@@ -0,0 +1,11 @@
+import { it, describe, expect } from "vitest";
+/*
+Example Integration Test to pass the CI Pipeline
+TO BE REMOVED AFTER ACTUAL INTEGRATION TESTS MADE
+*/
+
+describe("Example Frontend Integration Test", () => {
+	it("1 == 1", () => {
+		expect(1).toBe(1);
+	});
+});
