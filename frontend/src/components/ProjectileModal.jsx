001fcc3e3774f2793fe86839cbc9477b0322bbc5
Kelly Neuda <kvn904@mail.usask.ca>
2024-03-13 13:20:34 -0600

    Added adding a projectile point functionality to Jewel's attribute modals

--- frontend/src/components/ProjectileModal.jsx
+++ frontend/src/components/ProjectileModal.jsx
@@ -1,10 +1,8 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
-import Sidebar from "./Sidebar";
 import FileUpload from "./UploadPicture";
-import { Link, useLocation } from "react-router-dom";
-import Site from "./Site";
-import logger from "../logger.js";
+import { useLocation } from "react-router-dom";
+import log from "../logger.js";
 import {
 	Button,
 	Dialog,
@@ -14,18 +12,14 @@ import {
 	TextField,
 	MenuItem,
 	Grid,
-	FormControl,
 	IconButton,
 	Menu,
-	ListItemIcon,
-	ListItemText,
 } from "@mui/material";
 import EditIcon from "@mui/icons-material/Edit";
 import DeleteIcon from "@mui/icons-material/Delete";
 import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
-// import { EditIcon, DeleteIcon, MoreHorizIcon } from "@mui/icons-material";
-import log from "../logger.js";
 
+// Dropdown menu modal components
 import PeriodModal from "./PeriodModal.jsx";
 import CultureModal from "./CultureModal.jsx";
 import BaseShapeModal from "./BaseShapeModal.jsx";
@@ -37,19 +31,23 @@ import HaftingShapeModal from "./HaftingShapeModal.jsx";
 const AddProjectile = ({ setOpen }) => {
 	const inComingSiteInfo = useLocation();
 
-	const [siteID, setSiteID] = useState(inComingSiteInfo.state.info.id);
+	const siteID = inComingSiteInfo.state.info.id;
+	const siteName = inComingSiteInfo.state.info.name;
+
 	const [name, setName] = useState("");
-	const [description, setDescription] = useState("");
+	const [description, setDescription] = useState("Description"); // for testing
 	const [location, setLocation] = useState("");
-	const [dimensions, setDimensions] = useState("");
-	const [photoFilePath, setPhotoFilePath] = useState("");
-	const [subtype, setSubtype] = useState("");
-	const [artifactType, setArtifactType] = useState(1);
-	// const [cultureID, setCultureID] = useState(1);
-	// const [bladeShapeID, setBladeShapeID] = useState(1);
-	// const [baseShapeID, setBaseShapeID] = useState(1);
-	// const [haftingShapeID, setHaftingShapeID] = useState(1);
-	// const [crossSectionID, setCrossSectionID] = useState(1);
+	const [dimensions, setDimensions] = useState("2h x 4w x 7l"); // for testing
+	const [photoFilePath, setPhotoFilePath] = useState("/folder/images"); // for testing
+	// const [subtype, setSubtype] = useState("");
+	const [artifactTypeID, setartifactTypeID] = useState(0);
+	const [cultureID, setCultureID] = useState(0);
+	const [bladeShapeID, setBladeShapeID] = useState(0);
+	const [baseShapeID, setBaseShapeID] = useState(0);
+	const [haftingShapeID, setHaftingShapeID] = useState(0);
+	const [crossSectionID, setCrossSectionID] = useState(0);
+
+	const [periodID, setPeriodID] = useState(0); // not needed for adding projectile point, for testing only
 
 	// ------- For state variables for managing period dropdown and edit/delete functionalities -------
 	const [anchorEl, setAnchorEl] = useState(null); // For the dropdown menu anchor
@@ -117,7 +115,7 @@ const AddProjectile = ({ setOpen }) => {
 	const [selectedHaftingShapeID, setSelectedHaftingShapeID] = useState(null);
 	// -----------------------------------------------------------------------------------------
 
-	const [currentProjectiles, setCurrentProjectiles] = useState([]);
+	// const [currentProjectiles, setCurrentProjectiles] = useState([]);
 
 	// const PlaceholderText = "Add Information";
 	const handleClose = () => {
@@ -129,12 +127,12 @@ const AddProjectile = ({ setOpen }) => {
 		setName(event.target.value);
 	};
 
-	const handleLocationChange = (event) => {
-		setLocation(event.target.value);
-	};
-
 	const handleDescriptionChange = (event) => {
 		setDescription(event.target.value);
+	};
+
+	const handleLocationChange = (event) => {
+		setLocation(event.target.value);
 	};
 
 	const handleDimensionsChange = (event) => {
@@ -145,30 +143,28 @@ const AddProjectile = ({ setOpen }) => {
 		setPhotoFilePath(event.target.value);
 	};
 
-	const handleSubtypeChange = (event) => {
-		setSubtype(event.target.value);
-	};
+	// const handleSubtypeChange = (event) => {
+	// 	setSubtype(event.target.value);
+	// };
 
 	const handleSubmit = () => {
-		logger.info("Adding new projectile");
+		log.info("Adding new projectile");
 
 		const newProjectilePoint = {
-			name,
+			name: siteID + "-" + name,
 			location,
 			description,
 			dimensions,
 			photo: photoFilePath,
 			siteId: siteID,
-			artifactTypeId: artifactType,
-			subtype,
-			// cultureId,
-			// bladeShapdeId,
-			// baseShapeId,
-			// haftingShapeId,
-			// crossSectionId,
+			artifactTypeId: artifactTypeID,
+			// subtype,
+			cultureId: selectedCultureID,
+			bladeShapdeId: selectedBladeShapeID,
+			baseShapeId: selectedBaseShapeID,
+			haftingShapeId: selectedHaftingShapeID,
+			crossSectionId: selectedCrossSectionID,
 		};
-
-		logger.debug(newProjectilePoint);
 
 		axios
 			.post("http://localhost:3000/projectilePoints", newProjectilePoint)
@@ -198,10 +194,10 @@ const AddProjectile = ({ setOpen }) => {
 	// 	console.log(name);
 	// 	console.log(location);
 	// 	console.log(dimensions);
-	// 	console.log(artifactType);
+	// 	console.log(artifactTypeID);
 	// 	//setSiteID(some.id);
 	// 	// eslint-disable-next-line react-hooks/exhaustive-deps
-	// }, [dimensions, name, location, artifactType]);
+	// }, [dimensions, name, location, artifactTypeID]);
 
 	// ------------ For EDIT Period Modal ------------
 	// Load periods from the database on component mount
@@ -294,6 +290,7 @@ const AddProjectile = ({ setOpen }) => {
 
 	// This function the selectedCulture state when a user selects a different culture from the dropdown
 	const handleCultureChange = (event) => {
+		setCultureID(event.target.id);
 		setSelectedCulture(event.target.value);
 	};
 
@@ -598,9 +595,7 @@ const AddProjectile = ({ setOpen }) => {
 				fullWidth
 				PaperProps={{ style: { maxHeight: "80vh" } }}
 			>
-				<DialogTitle>
-					Add Projectile to site {inComingSiteInfo.state.info.name}
-				</DialogTitle>
+				<DialogTitle>Add Projectile Point to {siteName}</DialogTitle>
 				<DialogContent style={{ minHeight: "300px" }}>
 					<Grid container spacing={2} sx={{ paddingTop: 0 }}>
 						<Grid item xs={6}>
@@ -777,6 +772,61 @@ const AddProjectile = ({ setOpen }) => {
 								</Menu>
 							</TextField>
 							{/* ------------ End of BaseShapeModal  ------------- */}
+						</Grid>
+						<Grid item xs={6}>
+							{/*This should removed, as the location is attached to the site*/}
+							<TextField
+								margin="dense"
+								id="location"
+								label="Location"
+								fullWidth
+								value={location}
+								onChange={handleLocationChange}
+							/>
+							{/* The dimensions should be three different fields(length, width, height and )
+						if you are making this change, make sure the database was changed to hold a list of
+						float/double and not a string*/}
+							<TextField
+								margin="dense"
+								id="dimensions"
+								label="Dimensions"
+								fullWidth
+								value={dimensions}
+								onChange={handleDimensionsChange}
+							/>
+							{/* <FileUpload margin="dense" /> */}
+							<TextField
+								margin="dense"
+								id="photoFilePath"
+								label="Photo File Path"
+								fullWidth
+								value={photoFilePath}
+								onChange={handlePhotoFilePathChange}
+							/>
+							{/* <TextField
+								margin="dense"
+								id="subtype"
+								label="Subtype"
+								fullWidth
+								value={subtype}
+								onChange={handleSubtypeChange}
+							/> */}
+							{/*Should be renamed(maybe just drop the ID?)  also, Menu items will need to be dynamic at some point*/}
+							<TextField
+								margin="dense"
+								id="artifactTypeID"
+								label="ArtifactTypeID"
+								variant="outlined"
+								fullWidth
+								select
+								value={artifactTypeID}
+								onChange={(e) => setartifactTypeID(parseInt(e.target.value))}
+							>
+								<MenuItem value="1">Lithic</MenuItem>
+								<MenuItem value="2">Ceramic</MenuItem>
+								<MenuItem value="3">Faunal</MenuItem>
+								<MenuItem value="4">Shale</MenuItem>
+							</TextField>
 							{/* ------------ Start of CrossSectionModal  ------------- */}
 							<TextField
 								select
@@ -941,62 +991,6 @@ const AddProjectile = ({ setOpen }) => {
 								</Menu>
 							</TextField>
 							{/* ------------ End of HaftingShapeModal  ------------- */}
-						</Grid>
-						<Grid item xs={6}>
-							{/*This should removed, as the location is attached to the site*/}
-							<TextField
-								margin="dense"
-								id="location"
-								label="Location"
-								fullWidth
-								value={location}
-								onChange={handleLocationChange}
-							/>
-							{/* The dimensions should be three different fields(length, width, height and )
-						if you are making this change, make sure the database was changed to hold a list of
-						float/double and not a string*/}
-							<TextField
-								margin="dense"
-								id="dimensions"
-								label="Dimensions"
-								fullWidth
-								value={dimensions}
-								onChange={handleDimensionsChange}
-							/>
-							{/* <FileUpload margin="dense" /> */}
-							<TextField
-								margin="dense"
-								id="photoFilePath"
-								label="Photo File Path"
-								fullWidth
-								value={photoFilePath}
-								onChange={handlePhotoFilePathChange}
-							/>
-							<TextField
-								margin="dense"
-								id="subtype"
-								label="Subtype"
-								fullWidth
-								value={subtype}
-								onChange={handleSubtypeChange}
-							/>
-							{/*Should be renamed(maybe just drop the ID?)  also, Menu items will need to be dynamic at some point*/}
-							<TextField
-								margin="dense"
-								id="artifactTypeID"
-								label="ArtifactTypeID"
-								variant="outlined"
-								fullWidth
-								select
-								value={artifactType}
-								onChange={(e) => setArtifactType(e.target.value)}
-							>
-								<MenuItem value="1">Lithic</MenuItem>
-								<MenuItem value="2">Ceramic</MenuItem>
-								<MenuItem value="3">Faunal</MenuItem>
-								<MenuItem value="4">Hello</MenuItem>
-								<MenuItem value="5">Hi</MenuItem>
-							</TextField>
 						</Grid>
 					</Grid>
 				</DialogContent>
