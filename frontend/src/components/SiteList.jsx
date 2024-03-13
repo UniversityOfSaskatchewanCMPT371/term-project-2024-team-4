bda8ceb2184cc2724747b3696e4a3631d1b3c65c
lecegues <johnlecegues@gmail.com>
2024-03-13 16:40:13 -0600

    Added sorting functionality (newest-oldest, alphabetically)

--- frontend/src/components/SiteList.jsx
+++ frontend/src/components/SiteList.jsx
@@ -13,7 +13,7 @@ import {
 } from "@mui/material";
 import AddIcon from "@mui/icons-material/Add";
 
-export default function SiteList({ query }) {
+export default function SiteList({ query, sortValue }) {
 	const [open, setOpen] = useState(false);
 	const [data, setData] = useState([]);
 
@@ -34,10 +34,46 @@ export default function SiteList({ query }) {
 		// Fetch data from JSON server on component mount
 		fetch("http://localhost:3000/sites")
 			.then((response) => response.json())
-			.then((json) => setData(json))
+			.then((json) => {
+				// sort JSON first
+				const sortedData = sortData(json, sortValue);
+				setData(sortedData);
+			})
 			.catch((error) => console.error("Error fetching data:", error));
-	}, [open]);
+	}, [open, sortValue]);
 
+	/**
+	 * Sorts a JSON array given sorting criteria
+	 * Pre-Conditions:
+	 * 	- JSON objects need to contain a `createdDate` & `name` field
+	 *  - sortValue must be one of: newest, oldest, alphabetical_ascending, alphabetical_descending
+	 * Post-Conditions:
+	 *   - Original JSON data is kept but is sorted based on the given sorting criteria
+	 * @param {Object[]} data - array of JSON objects to be sorted
+	 * @param {string} sortValue - criteria to be sorted: "newest", "oldest", "alphabetical_ascending", "alphabetical_descending"
+	 * @returns {Object[]} a sorted array of JSON objects
+	 */
+	function sortData(data, sortValue) {
+		return data.sort((a, b) => {
+			switch (sortValue) {
+				case "newest":
+					console.info("Sorting sites from newest-oldest order");
+					return new Date(b.createdDate) - new Date(a.createdDate);
+				case "oldest":
+					console.info("Sorting sites from oldest-newest order");
+					return new Date(a.createdDate) - new Date(b.createdDate);
+				case "alphabetical_ascending":
+					console.info("Sorting sites in alphabetically ascending order");
+					return a.name.localeCompare(b.name);
+				case "alphabetical_descending":
+					console.info("Sorting sites in alphabetically descending order");
+					return b.name.localeCompare(a.name);
+				default:
+					console.warn("Could not sort sites.");
+					return 0;
+			}
+		});
+	}
 	//Filter data based on search query (mock)
 	const filteredData = data?.filter((item) =>
 		item.name.toLowerCase().includes(query.toLowerCase()),
