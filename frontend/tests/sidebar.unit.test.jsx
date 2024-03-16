45faac98f398bac856907eb2ad07bd5813bec910
Kelly Neuda <kvn904@mail.usask.ca>
2024-03-16 16:47:05 -0600

    Copied unit tests from ID2

---
+++ frontend/tests/sidebar.unit.test.jsx
@@ -0,0 +1,29 @@
+import { describe, it, expect } from "vitest";
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import Sidebar from "../src/components/Sidebar.jsx";
+import { MemoryRouter } from "react-router-dom";
+
+describe("Sidebar", () => {
+	it("renders correctly", () => {
+		render(
+			<MemoryRouter>
+				<Sidebar />
+			</MemoryRouter>,
+		);
+
+		expect(screen.getByText(/pcubed/i)).toBeInTheDocument();
+		expect(screen.getByTestId("HomeIcon")).toBeInTheDocument();
+		expect(screen.getByText(/home/i)).toBeInTheDocument();
+		expect(screen.getByTestId("UploadIcon")).toBeInTheDocument();
+		expect(screen.getByText(/connect/i)).toBeInTheDocument();
+		expect(screen.getByTestId("BarChartIcon")).toBeInTheDocument();
+		expect(screen.getByText(/statistics/i)).toBeInTheDocument();
+		expect(screen.getByTestId("FolderCopyIcon")).toBeInTheDocument();
+		expect(screen.getByText(/data management/i)).toBeInTheDocument();
+		expect(screen.getByTestId("RoomPreferencesIcon")).toBeInTheDocument();
+		expect(screen.getByText(/settings/i)).toBeInTheDocument();
+		expect(screen.getByTestId("LoginIcon")).toBeInTheDocument();
+		expect(screen.getByText(/login/i)).toBeInTheDocument();
+	});
+});
