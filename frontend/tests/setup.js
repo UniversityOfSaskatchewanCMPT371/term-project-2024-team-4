6a60cecfee1ef07cb84b07d0c283162b5ada6f43
ams872 <ams872@usask.ca>
2024-03-16 13:03:50 -0600

    Fixing issue with unit test involving opening modal not being able to run, added window.matchMedia to setup.js. Removed redundant test

--- frontend/tests/setup.js
+++ frontend/tests/setup.js
@@ -1,4 +1,4 @@
-import { expect, afterEach } from "vitest";
+import { expect, afterEach, vi } from "vitest";
 import { cleanup } from "@testing-library/react";
 import * as matchers from "@testing-library/jest-dom/matchers";
 import "@testing-library/jest-dom";
@@ -7,4 +7,18 @@ expect.extend(matchers);
 
 afterEach(() => {
 	cleanup();
+});
+
+Object.defineProperty(window, "matchMedia", {
+	writable: true,
+	value: vi.fn().mockImplementation((query) => ({
+		matches: false,
+		media: query,
+		onchange: null,
+		addListener: vi.fn(), // deprecated
+		removeListener: vi.fn(), // deprecated
+		addEventListener: vi.fn(),
+		removeEventListener: vi.fn(),
+		dispatchEvent: vi.fn(),
+	})),
 });
