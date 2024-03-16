7118ce6f769251f8a4a57e202d31a6b8fdbb03ed
m-jewel <wzt206@usask.ca>
2024-03-16 14:35:02 -0600

    Fixed problem where Sidebar components did not load due to refactoring done earilier

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -2,14 +2,24 @@ import { useState, useEffect } from "react";
 import logger from "../logger.js";
 import LoginModal from "./LoginModal";
 import axios from "axios";
-// MUI components
+import { Link } from "react-router-dom";
+// MUI
 import Drawer from "@mui/material/Drawer";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import List from "@mui/material/List";
-
+import Divider from "@mui/material/Divider";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
 import ListItemIcon from "@mui/material/ListItemIcon";
-// MUI icons
+import ListItemText from "@mui/material/ListItemText";
+import HomeIcon from "@mui/icons-material/Home";
+import UploadIcon from "@mui/icons-material/Upload";
+import BarChartIcon from "@mui/icons-material/BarChart";
+import FolderCopyIcon from "@mui/icons-material/FolderCopy";
+import RoomPreferencesIcon from "@mui/icons-material/RoomPreferences";
+import LoginIcon from "@mui/icons-material/Login";
+import ExitToAppIcon from "@mui/icons-material/ExitToApp";
 import { styled } from "@mui/material/styles";
 
 const drawerWidth = 240;
@@ -139,6 +149,7 @@ function Sidebar() {
 				}}
 				PaperProps={{
 					sx: {
+						// sidebar background color
 						backgroundColor: "#f1f1f1",
 					},
 				}}
@@ -150,8 +161,74 @@ function Sidebar() {
 						PCubed
 					</Typography>
 				</Toolbar>
-				<SidebarList>{/* List of navigation links and actions */}</SidebarList>
-				{/* More lists and login/logout actions */}
+				<SidebarList>
+					<Link to="/" style={{ textDecoration: "none", color: "inherit" }}>
+						<ListItem key="Home" disablePadding onClick={handleClick}>
+							<ListItemButton>
+								<SidebarIcon>
+									<HomeIcon />
+								</SidebarIcon>
+								<ListItemText primary="Home" />
+							</ListItemButton>
+						</ListItem>
+					</Link>
+					<ListItem key="Connect" disablePadding onClick={handleClick}>
+						<ListItemButton>
+							<SidebarIcon>
+								<UploadIcon />
+							</SidebarIcon>
+							<ListItemText primary="Connect" />
+						</ListItemButton>
+					</ListItem>
+				</SidebarList>
+				<Divider />
+				<SidebarList>
+					<ListItem key="Statistics" disablePadding onClick={handleClick}>
+						<ListItemButton>
+							<SidebarIcon>
+								<BarChartIcon />
+							</SidebarIcon>
+							<ListItemText primary="Statistics" />
+						</ListItemButton>
+					</ListItem>
+					<ListItem key="Data Management" disablePadding onClick={handleClick}>
+						<ListItemButton>
+							<SidebarIcon>
+								<FolderCopyIcon />
+							</SidebarIcon>
+							<ListItemText primary="Data Management" />
+						</ListItemButton>
+					</ListItem>
+				</SidebarList>
+				<SidebarList sx={{ marginTop: "auto" }}>
+					<ListItem key="Settings" disablePadding onClick={handleClick}>
+						<ListItemButton>
+							<SidebarIcon>
+								<RoomPreferencesIcon />
+							</SidebarIcon>
+							<ListItemText primary="Settings" />
+						</ListItemButton>
+					</ListItem>
+					{isLoggedIn ? (
+						<ListItem key="Logout" disablePadding onClick={handleLogout}>
+							<ListItemButton>
+								<SidebarIcon>
+									<ExitToAppIcon />
+								</SidebarIcon>
+								<ListItemText primary="Logout" />
+							</ListItemButton>
+						</ListItem>
+					) : (
+						<ListItem key="Login" disablePadding onClick={setModalVisible}>
+							<ListItemButton>
+								<SidebarIcon>
+									<LoginIcon />
+								</SidebarIcon>
+								<ListItemText primary="Login" />
+							</ListItemButton>
+						</ListItem>
+					)}
+				</SidebarList>
 			</Drawer>
 			<LoginModal modalVisible={modalVisible} closeModal={closeModal} />
 		</>
