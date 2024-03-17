0c3a78af3652799edfb1db0d6bd58f0b22d33e90
lecegues <johnlecegues@gmail.com>
2024-03-17 00:09:58 -0600

    Added more unit tests

--- backend/tests/config/sanitize.unit.test.js
+++ backend/tests/config/sanitize.unit.test.js
@@ -35,6 +35,12 @@ app.post("/artifact", artifactValidationRules(), validate, (req, res) =>
 app.post("/period", periodValidationRules(), validate, (req, res) =>
 	res.status(200).json({ message: "OK" }),
 );
+app.post("/nameOnly", nameValidationRules(), validate, (req, res) =>
+	res.status(200).json({ message: "OK" }),
+);
+app.post("/nameDescOnly", nameDescValidationRules(), validate, (req, res) =>
+	res.status(200).json({ message: "OK" }),
+);
 
 // All tests are for sanitization & validation checks
 describe("Sanitization and Validation Checks", () => {
@@ -106,6 +112,140 @@ describe("Sanitization and Validation Checks", () => {
 
 			// should return error response because of invalid symbols
 			expect(response.statusCode).toBe(422);
+		});
+	});
+
+	// Tests for Sites API
+	describe("Sites Sanitization & Validation", () => {
+		test("should reject if name is not included", async () => {
+			const response = await request(app).post("/site").send({
+				name: "",
+				description: "description",
+				location: "location",
+			});
+			expect(response.statusCode).toBe(422);
+		});
+
+		test("should accept valid name with empty description/location", async () => {
+			const response = await request(app).post("/site").send({
+				name: "name",
+				description: "",
+				location: "",
+			});
+			expect(response.statusCode).toBe(200);
+		});
+
+		test("should accept if all fields are valid", async () => {
+			const response = await request(app).post("/site").send({
+				name: "Name",
+				description: "descriptionTest",
+				location: "Location",
+			});
+			expect(response.statusCode).toBe(200);
+		});
+	});
+
+	// Tests for Artifact API
+	describe("Artifact Sanitization & Validation", () => {
+		test("should reject if name is not included", async () => {
+			const response = await request(app).post("/artifact").send({
+				name: "",
+				description: "desc",
+				location: "",
+				dimensions: "",
+				photo: "",
+			});
+
+			expect(response.statusCode).toBe(422);
+		});
+
+		test("should accept even if fields other than name are not included", async () => {
+			const response = await request(app).post("/artifact").send({
+				name: "Name",
+				description: "",
+				location: "",
+				dimensions: "",
+				photo: "",
+			});
+			expect(response.statusCode).toBe(200);
+		});
+
+		test("should accept if all fields are valid", async () => {
+			const response = await request(app).post("/artifact").send({
+				name: "Name",
+				description: "Desc",
+				location: "Loc",
+				dimensions: "Dim",
+				photo: "Phot",
+			});
+			expect(response.statusCode).toBe(200);
+		});
+	});
+	// Tests for Period API
+	describe("Period Sanitization & Validation", () => {
+		test("should reject if name is not included", async () => {
+			const response = await request(app).post("/period").send({
+				name: "",
+			});
+
+			expect(response.statusCode).toBe(422);
+		});
+
+		test("should accept if all fields valid", async () => {
+			const response = await request(app).post("/period").send({
+				name: "Name",
+			});
+
+			expect(response.statusCode).toBe(200);
+		});
+	});
+
+	// Tests for name-only needed APIs
+	describe("Name-Only Sanitization & Validation", () => {
+		test("should reject if name is not included", async () => {
+			const response = await request(app).post("/nameOnly").send({
+				name: "",
+			});
+
+			expect(response.statusCode).toBe(422);
+		});
+
+		test("should accept if all fields valid", async () => {
+			const response = await request(app).post("/nameOnly").send({
+				name: "Name",
+			});
+
+			expect(response.statusCode).toBe(200);
+		});
+	});
+
+	// Tests for name & desc only needed APIs
+	describe("Name & Desc Only Sanitization & Validation", () => {
+		test("should reject if name is not included", async () => {
+			const response = await request(app).post("/nameDescOnly").send({
+				name: "",
+				description: "Desc",
+			});
+
+			expect(response.statusCode).toBe(422);
+		});
+
+		test("should accept even if description is empty", async () => {
+			const response = await request(app).post("/nameDescOnly").send({
+				name: "Name",
+				description: "",
+			});
+
+			expect(response.statusCode).toBe(200);
+		});
+
+		test("should accept if all fields valid", async () => {
+			const response = await request(app).post("/nameDescOnly").send({
+				name: "Name",
+				description: "Desc",
+			});
+
+			expect(response.statusCode).toBe(200);
 		});
 	});
 });
