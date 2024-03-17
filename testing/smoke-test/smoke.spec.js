0c4756f1b10c77b6d5307c58e1aeadce79c34001
brj496 <brj496@usask.ca>
2024-03-16 22:42:45 -0600

    Adds integration, end to end, and smoke tests for the aggregateStatisticsGenerator feature. (note the statisitcsPage.spec.js e2e tests are in the smoke test folder as thats the only place they would function)

--- testing/smoke-test/smoke.spec.js
+++ testing/smoke-test/smoke.spec.js
@@ -1,5 +1,5 @@
 // @ts-check
-const { test, expect} = require('@playwright/test');
+const { test, expect } = require("@playwright/test");
 /**
  * This file will host our smoke tests to ensure that
  * the main functionality of the system works.
@@ -9,13 +9,12 @@ const { test, expect} = require('@playwright/test');
 /*
 Test that you can succesfully go to page with proper title (e.g. page exists)
 */
-test('has title', async ({ page }) => {
-    await page.goto('http://localhost:8080');
-  
-    // Expect the correct title
-    await expect(page).toHaveTitle("Vite + React");
-  });
+test("has title", async ({ page }) => {
+	await page.goto("http://localhost:8080");
 
+	// Expect the correct title
+	await expect(page).toHaveTitle("Vite + React");
+});
 
 /*
 Admin Login
@@ -25,65 +24,64 @@ Admin Login
         - Test admin-only endpoint after login
 */
 test.describe("Login Smoke Tests", () => {
+	// test to determine proper response HTTP status codes upon login attempt
+	test("Login process with failure and success scenarios", async ({ page }) => {
+		// Navigate to login page
+		await page.goto("http://localhost:8080");
 
-    // test to determine proper response HTTP status codes upon login attempt
-    test('Login process with failure and success scenarios', async ({ page }) => {
-        // Navigate to login page
-        await page.goto("http://localhost:8080");
+		// Find and press Login Button
+		const loginButton = page.locator("text=Login");
+		await expect(loginButton).toBeVisible();
+		await loginButton.click();
 
-        // Find and press Login Button
-        const loginButton = page.locator('text=Login');
-        await expect(loginButton).toBeVisible();
-        await loginButton.click();
+		// Define credentials for testing
+		// TC01 to TC08
+		const credentials = [
+			{ username: "admin123#.;", password: "admin" },
+			{ username: "ADMIN", password: "admin" },
+			{ username: "admin", password: "admin123#.;" },
+			{ username: "admin", password: "ADMIN" },
+			{ username: "", password: "" },
+			{ username: "admin", password: "" },
+			{ username: "", password: "admin" },
+			{ username: "admin", password: "admin" },
+		];
 
-        // Define credentials for testing 
-        // TC01 to TC08
-        const credentials = [
-            { username: 'admin123#.;', password: "admin"},
-            { username: 'ADMIN', password: "admin"},
-            { username: 'admin', password: "admin123#.;"},
-            { username: 'admin', password: "ADMIN"},
-            { username: '', password: ""},
-            { username: 'admin', password: ""},
-            { username: '', password: "admin"},
-            { username: 'admin', password: "admin"},
+		// Use credentials to attempt login
+		for (const credential of credentials) {
+			// Fill the inputs
+			await page.fill('input[name="username"]', credential.username);
+			await page.fill('input[name="password"]', credential.password);
 
-        ];
+			await page.waitForLoadState("networkidle");
 
-        // Use credentials to attempt login
-        for (const credential of credentials) {
+			// Submit login
+			var [response] = await Promise.all([
+				page.waitForResponse(
+					(resp) =>
+						resp.url().includes("/users") && resp.request().method() == "POST",
+				),
+				await page.locator("[type=submit]").click(),
+			]);
 
-            // Fill the inputs
-            await page.fill('input[name="username"]', credential.username);
-            await page.fill('input[name="password"]', credential.password);
+			// Expect the right response
+			try {
+				var responseStatus = response.status();
 
-            await page.waitForLoadState('networkidle')
-
-            // Submit login 
-            var [response] = await Promise.all([
-                page.waitForResponse(resp => resp.url().includes('/users') && resp.request().method() == "POST"),
-                await page.locator("[type=submit]").click()
-            ]);
-            
-            // Expect the right response
-            try {
-                var responseStatus = response.status();
-
-                if (credential.username === 'admin' && credential.password === 'admin'){
-                    // expect succesful response
-                    await expect(responseStatus).toBe(200);
-                }
-                else{
-                    await expect([400,401]).toContain(responseStatus);
-                }
-            }
-            catch (error){
-                console.error("Error parsing response JSON", error);
-            }
-            
-            
-        }
-    })
+				if (
+					credential.username === "admin" &&
+					credential.password === "admin"
+				) {
+					// expect succesful response
+					await expect(responseStatus).toBe(200);
+				} else {
+					await expect([400, 401]).toContain(responseStatus);
+				}
+			} catch (error) {
+				console.error("Error parsing response JSON", error);
+			}
+		}
+	});
 });
 
 /*
@@ -98,8 +96,50 @@ Import/Export
 */
 
 /*
-Data Aggregation
+Aggregate Statistics
+    - Can get to the Page
+    - Can open generate statisitics 
+    - Can close generate statistics
 */
+test.describe("Aggregate Statisitics Tests", () => {
+	//NOTE: The button to get to the stats page is non funcitonal, uncomment this once its implemented.
+	// test("Get to the Statistics Page from the home page.", async ({ page }) => {
+	// 	await page.goto("http://localhost:8080/");
+
+	// 	const statisticsButton = page.locator("text=Statistics");
+	// 	await expect(statisticsButton).toBeVisible();
+	// 	await statisticsButton.click();
+
+	// 	await expect(page).toHaveURL("http://localhost:8080/stats");
+	// });
+
+	test("Get to the statistics modal from the statistics page.", async ({
+		page,
+	}) => {
+		await page.goto("http://localhost:8080/stats");
+
+		const generateButton = page.locator("text=GENERATE STATISTICS");
+		await expect(generateButton).toBeVisible();
+		await generateButton.click();
+
+		const modalWindow = await page.getByRole("dialog", { name: "Statistics" });
+		await expect(modalWindow).toBeVisible();
+	});
+
+	test("Can close generate statistics modal", async ({ page }) => {
+		await page.goto("http://localhost:8080/stats");
+
+		const generateButton = page.locator("text=GENERATE STATISTICS");
+		await expect(generateButton).toBeVisible();
+		await generateButton.click();
+
+		const closeButton = page.locator("text=CLOSE");
+		await expect(closeButton).toBeVisible();
+
+		const modalWindow = await page.getByRole("dialog", { name: "Statistics" });
+		await expect(modalWindow).toBeHidden();
+	});
+});
 
 /*
 Search/Filter
