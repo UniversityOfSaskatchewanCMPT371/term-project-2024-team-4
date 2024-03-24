2a11e743d57f390d9fe2b72cc832ca621fcf8de2
Kelly Neuda <kvn904@mail.usask.ca>
2024-03-24 15:39:12 -0600

    Line removed due to redundancy with line 6, as requested in pull request review

--- frontend/tests/setup.js
+++ frontend/tests/setup.js
@@ -4,7 +4,6 @@ import * as matchers from "@testing-library/jest-dom/matchers";
 import "@testing-library/jest-dom";
 import axios from "axios";
 axios.defaults.adapter = "http";
-axios.defaults.baseURL = "http://127.0.0.1:3000";
 
 expect.extend(matchers);
 
