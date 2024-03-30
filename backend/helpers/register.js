be5bb64d224a63c1ae28d6e17762ce487ce061e3
lecegues <johnlecegues@gmail.com>
2024-03-30 03:34:47 -0600

    Updated `register.js` to be a proper helper file and take inputs rather than hardcoding credentials

--- backend/helpers/register.js
+++ backend/helpers/register.js
@@ -4,29 +4,32 @@ const bcrypt = require("bcrypt");
 const { logger } = require("../config/logger");
 
 /**
- * registerUser
- *
- * Purpose: Register a new user with hardcoded username and password.
- *
- * Pre-conditions: None
- *
- * Post-conditions:
- * - Registers a new user with the hardcoded username and password.
- * - Returns void.
+ * Registers a new user with the given username and password
+ * @param {*} username - username of the new user
+ * @param {*} password - password of the new user
+ * @precond
+ * 	- Username is unique
+ * @postcond registers a new user with username and password
  */
-const registerUser = async () => {
+const registerUser = async (username, password) => {
 	const Users = await dataSource.getRepository(User);
-	// hardcoded data
 
-	// could be a GitHub secret
-	const hardcodedUserName = "admin";
-	const hardcodedPassword = "admin";
+	// first check if username is unique
+	const userExists = await Users.findOneBy({ userName: username });
+	if (userExists) {
+		logger.warn(`Trying to register user: ${username} already exists.`);
+		return;
+	}
 
+	// hash password and create new user
+	const hashedPassword = await bcrypt.hash(password, 10);
 	const newUser = new User();
-	newUser.userName = hardcodedUserName;
-	newUser.password = await bcrypt.hash(hardcodedPassword, 10);
+	newUser.userName = username;
+	newUser.password = hashedPassword;
+
+	// save user to database
 	await Users.save(newUser);
-	logger.info("User successfully registered");
+	logger.info(`User ${username} succesfully registered`);
 	return;
 };
 
