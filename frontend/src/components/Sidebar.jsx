394e9d1eb403c9d81e897c2ac0ab07c913ac7c3d
Kelly Neuda <kvn904@mail.usask.ca>
2024-04-03 02:31:28 -0600

    Fixed linting errors to pass code standards

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -8,7 +8,6 @@ import Drawer from "@mui/material/Drawer";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import List from "@mui/material/List";
-import Divider from "@mui/material/Divider";
 import ListItem from "@mui/material/ListItem";
 import ListItemButton from "@mui/material/ListItemButton";
 import ListItemIcon from "@mui/material/ListItemIcon";
